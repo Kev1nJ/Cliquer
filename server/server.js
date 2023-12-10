ed1a530b31f1af540d4033908d9e1668e01cdf07
Kev1nJ <kevinjerome85@yahoo.com>
2023-12-09 20:04:53 -0500

    server file correct , moduels imported and erorr handling,  html need

--- server/server.js
+++ server/server.js
@@ -1,19 +1,50 @@
-require("dotenv").config()
-
-const express = require("express")
+// app.js
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
 const app = express();
 
-const PORT = process.env.PORT || 3005;
-const router = require("../routes")
+require('dotenv').config();
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-
-const db = require('./config/connection')
-
-app.use("/", router)
-
-db.once('open', () => {
-    app.listen(PORT, () => console.log(`Server is listening port ${PORT}`))
+// Connect to the MongoDB database
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.error('MongoDB connection error:', err));
 
+app.use((req, res, next) => {
+  console.log(`Received ${req.method} request to ${req.url}`);
+  next();
+});
+
+app.use(express.json()); // Parse JSON bodies
+app.use(cors());
+
+// API routes
+const authRoutes = require('./routes/auth');
+const recipeRoutes = require('./routes/recipe');
+
+app.use('/auth', authRoutes);
+app.use('/recipe', recipeRoutes);
+
+// Root route handler
+app.get('/', (req, res) => {
+  res.send('Recipe Searching App!');
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+
+  res.status(500).json({
+    status: 'error',
+    message: 'Something went wrong!',
+  });
+});
+
+const port = process.env.PORT || 3006;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
