62358e374fff968b73fb46fe161e7998faf36d03
Kevin Jerome <137011939+Kev1nJ@users.noreply.github.com>
2023-12-10 21:22:06 -0500

    Update SearchBar.jsx

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -1,18 +1,18 @@
-import React from 'react';
+// import React from 'react';
 
-const SearchBar = () => {
+// const SearchBar = () => {
   
-  const handleSearch = (query) => {
-    console.log(`Searching for: ${query}`);
+//   const handleSearch = (query) => {
+//     console.log(`Searching for: ${query}`);
    
-  };
+//   };
 
-  return (
-    <div>
-      <input type="text" placeholder="Search..." />
-      <button onClick={() => handleSearch("user entered query")}>Search</button>
-    </div>
-  );
-};
+//   return (
+//     <div>
+//       <input type="text" placeholder="Search..." />
+//       <button onClick={() => handleSearch("user entered query")}>Search</button>
+//     </div>
+//   );
+// };
 
-export default SearchBar;
+// export default SearchBar;
