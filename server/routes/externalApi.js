fc7bf79ee6e4e37f49043ae9566f48771e969cae
Kevin Jerome <137011939+Kev1nJ@users.noreply.github.com>
2023-12-10 21:32:48 -0500

    Add files via upload

---
+++ server/routes/externalApi.js
@@ -0,0 +1,9 @@
+// // routes/externalApi.js
+// const express = require('express');
+// const router = express.Router();
+// const externalApiController = require('../controllers/externalApi');
+
+// // GET /external-api/recipes
+// router.get('/recipes', externalApiController.getRecipesFromExternalApi);
+
+// module.exports = router;
