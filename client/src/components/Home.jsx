f811f3feda6aa589a83fd35d07b3c6f668d595de
Kevin Jerome <137011939+Kev1nJ@users.noreply.github.com>
2023-12-10 21:21:30 -0500

    Update Home.jsx

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
+// import React from 'react';
 
-const Home = () => {
-  return (
-    <div>
-      <h2>Welcome to Your Recipe App!</h2>
-      {/* Add main page content here */}
-    </div>
-  );
-};
+// const Home = () => {
+//   return (
+//     <div>
+//       <h2>Welcome to Your Recipe App!</h2>
+//       {/* Add main page content here */}
+//     </div>
+//   );
+// };
 
-export default Home;
+// export default Home;
