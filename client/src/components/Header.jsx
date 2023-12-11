651b747a69c1f61e12c5d6801bbb578fbedb18da
Kevin Jerome <137011939+Kev1nJ@users.noreply.github.com>
2023-12-10 21:21:17 -0500

    Update Header.jsx

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import SearchBar from './SearchBar';
+// import React from 'react';
+// import SearchBar from './SearchBar';
 
-const Header = () => {
-  return (
-    <header>
-      <h1>Your Recipe App</h1>
-      <SearchBar />
-    </header>
-  );
-};
+// const Header = () => {
+//   return (
+//     <header>
+//       <h1>Your Recipe App</h1>
+//       <SearchBar />
+//     </header>
+//   );
+// };
 
-export default Header;
+// export default Header;
