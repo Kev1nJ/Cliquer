b80d437687d54adc3f31f35a6e8c8c01c329058d
Kev1nJ <kevinjerome85@yahoo.com>
2023-12-10 22:28:50 -0500

    recipe delete routes and controller added

--- server/controllers/recipe.js
+++ server/controllers/recipe.js
@@ -1,5 +1,5 @@
-// controllers/recipe.js
 const User = require('../models/User');
+const Recipe = require('../models/Recipe');
 
 // Save a recipe to user's profile
 exports.saveRecipe = async (req, res) => {
@@ -28,6 +28,37 @@ exports.saveRecipe = async (req, res) => {
     res.status(201).json({ message: 'Recipe saved successfully' });
   } catch (error) {
     console.error('Error saving recipe:', error);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+};
+
+// Delete a saved recipe from user's profile
+exports.deleteRecipe = async (req, res) => {
+  try {
+    const userId = req.userId; // Assuming you have middleware to extract user ID from JWT
+    const { recipeId } = req.body;
+
+    // Find the user by ID
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    // Check if the recipe is saved by the user
+    const savedRecipeIndex = user.savedRecipes.findIndex((recipe) => recipe.recipeId === recipeId);
+
+    if (savedRecipeIndex === -1) {
+      return res.status(404).json({ message: 'Recipe not found in user\'s saved recipes' });
+    }
+
+    // Remove the recipe from the user's saved recipes
+    user.savedRecipes.splice(savedRecipeIndex, 1);
+    await user.save();
+
+    res.status(200).json({ message: 'Recipe deleted successfully' });
+  } catch (error) {
+    console.error('Error deleting recipe:', error);
     res.status(500).json({ message: 'Internal Server Error' });
   }
 };
