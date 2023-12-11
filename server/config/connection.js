74d84e91498c9a26920706f0b3b9a1e74ddb0703
Kevin Jerome <137011939+Kev1nJ@users.noreply.github.com>
2023-12-10 21:24:11 -0500

    Update connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,16 @@
-const mongoose = require("mongoose");
+// Connect to the MongoDB database
+const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGOURI || 'mongodb://localhost:27017/bootcamp')
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.error('MongoDB connection error:', err));
+
+app.use((req, res, next) => {
+  console.log(`Received ${req.method} request to ${req.url}`);
+  next();
+});
 
 module.exports = mongoose.connection;
