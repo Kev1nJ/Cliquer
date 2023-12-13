97b0a87f74589e0e1a2098be1c1455d9af71cfe5
Kev1nJ <kevinjerome85@yahoo.com>
2023-12-12 19:27:06 -0500

    auth and routes needed for login ,logout regisdter etc, need mongoose connections resolved

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 // Connect to the MongoDB database
 const mongoose = require('mongoose');
 require('dotenv').config();
-//dotenv
+
 
 
 console.log(process.env.MONGODB_URI)
@@ -13,9 +13,5 @@ mongoose.connect(process.env.MONGODB_URI, {
   .then(() => console.log('Connected to MongoDB'))
   .catch((err) => console.error('MongoDB connection error:', err));
 
-// app.use((req, res, next) => {
-//   console.log(`Received ${req.method} request to ${req.url}`);
-//   next();
-// });
 
 module.exports = mongoose.connection;
--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -5,8 +5,28 @@ const jwt = require('jsonwebtoken');
 
 
 let register = async (req, res) => {
-  res.json({message:"test"})
-}
+  try {
+    const { email, password } = req.body;
+
+    // Check if the user already exists
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: 'User already exists' });
+    }
+
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Create a new user
+    const newUser = new User({ email, password: hashedPassword });
+    await newUser.save();
+
+    res.status(201).json({ message: 'User registered successfully' });
+  } catch (error) {
+    console.error('Error registering user:', error);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+};
 
 let login = async (req, res) => {
   try {
