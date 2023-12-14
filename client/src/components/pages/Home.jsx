396b7f8f99da3b29040479183c3291b010098dfd
Kev1nJ <kevinjerome85@yahoo.com>
2023-12-13 22:53:01 -0500

    a tags change to Links in Nav component

--- client/src/components/pages/Home.jsx
+++ client/src/components/pages/Home.jsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import stockImage1 from '../../../public/images/anna-tukhfatullina-food-photographer-stylist-Mzy-OjtCI70-unsplash.jpg';
 import stockImage2 from '../../../public/images/casey-lee-awj7sRviVXo-unsplash.jpg';
 import stockImage3 from '../../../public/images/chad-montano-MqT0asuoIcU-unsplash.jpg';
 import stockImage4 from '../../../public/images/joseph-gonzalez-zcUgjyqEwe8-unsplash.jpg';
-
+import SearchBar from './SearchBar';
 
 const Home = () => {
+  const [searchResults, setSearchResults] = useState([]);
+
+  // Function to update search results
+  const updateSearchResults = (results) => {
+    setSearchResults(results);
+  };
+
   return (
     <section id="home">
       {/* Add stock images */}
@@ -19,6 +26,19 @@ const Home = () => {
       <img id="stockImage" src={stockImage4} alt="Stock Image 4"
       style={{ width: '100%', maxWidth: '400px', margin: '0 auto' }} />
     </div>
+    <SearchBar updateSearchResults={updateSearchResults} />
+
+      {/* Display search results */}
+      {searchResults.length > 0 && (
+        <div id="searchResults">
+          <h2>Search Results:</h2>
+          <ul>
+            {searchResults.map((result) => (
+              <li key={result.id}>{result.title}</li>
+            ))}
+          </ul>
+        </div>
+      )}
     <div id="homeDiv">
       <h2>SEARCH FOR YOUR RECIPE</h2>
       <h2>EXPLORE THE POSSIBILITIES!</h2>
